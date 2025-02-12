f21a579410fd6180fba8fb4d1cd7ca66c534f116
makerGirl123 <mercyme04@hotmail.com>
2025-02-11 19:32:47 -0500

    multiple screens and back button

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-from kivymd.app import MDApp
+from kivy.uix.behaviors import TouchRippleBehavior
 from kivymd.uix.label import MDLabel, MDIcon
-from kivymd.font_definitions import theme_font_styles
-from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRectangleFlatButton
 
+# used for most MD apps
+from kivymd.app import MDApp
+#Used to reference the widgets in .kv and keep the positions
+from kivy.uix.floatlayout import FloatLayout
+#Used for changing screens
+from kivy.uix.screenmanager import ScreenManager, Screen
 #used to set screen size
 from kivy.core.window import Window
 
 #example Android screen size
 Window.size = (412, 896)
 
+
+#class for each screen and its' canvas
+class HomeScreen(Screen):
+    pass
+    class HomeScreenCanvas(FloatLayout):
+        pass
+
+class HobbiesScreen(Screen):
+    pass
+    class HobbiesScreenCanvas(FloatLayout):
+        pass
+
+class FavoritesScreen(Screen):
+    pass
+    class FavoritesScreenCanvas(FloatLayout):
+        pass
 
 class AboutMeApp(MDApp):
     def build(self):
@@ -19,28 +39,18 @@ class AboutMeApp(MDApp):
         #self.theme_cls.theme_style = "Light"
 
         #Dark Mode
-        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_palette = "Purple"
         self.theme_cls.primary_hue = "400"
         self.theme_cls.theme_style = "Dark"
 
-        #Create the screen object
-        #screen = Screen()
+        self.sm = ScreenManager()
+        self.sm.add_widget(HomeScreen(name='home'))
+        self.sm.add_widget(HobbiesScreen(name='hobbies'))
+        self.sm.add_widget(FavoritesScreen(name='favorites'))
+        return self.sm
 
-        #Create a button
-        #btn_flat = MDRectangleFlatButton(text='Hello World', pos_hint={'center_x':0.5, 'center_y':0.5})
-
-
-        #Create a Label
-        #label = MDLabel(text="All About Me", pos_hint={'center_x':0.5, 'center_y':0.8}, theme_text_color="Secondary", font_style="H2")
-
-        #If Declarative: Adding widgets to the screen
-        #screen.add_widget(label)
-        #screen.add_widget(btn_flat)
-
-
-
-
-
+    def go_home(self):
+        self.sm.current = 'home'
 
 if __name__ == '__main__':
     AboutMeApp().run()
